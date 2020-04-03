d95733016d51b88a6840ca06ca610e4104ce00b9
刘继强 <2265940560@qq.com>
2020-04-03 18:42:23 +0800

    Update UploadUtil.java

--- QuestionBankBackend/olcp-consumer/src/main/java/olcp/utils/UploadUtil.java
+++ QuestionBankBackend/olcp-consumer/src/main/java/olcp/utils/UploadUtil.java
@@ -16,14 +16,14 @@ public class UploadUtil {
         String endpoint = "oss-cn-qingdao.aliyuncs.com";
         // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录
         // https://ram.console.aliyun.com 创建
-        String accessKeyId = "LTAIHgWkDSnmp3oE";
-        String accessKeySecret = "modt7r4rvPPK4zpcfwpLUilz8nnH7O";
+        String accessKeyId = "xxxxxxxxx";
+        String accessKeySecret = "xxxxxxxx";
         // 创建OSSClient实例
         OSSClient ossClient = new OSSClient(endpoint, accessKeyId, accessKeySecret);
         // 上传
-        ossClient.putObject("ljqzq", name, file);
+        ossClient.putObject("xxxxx", name, file);
         Date expiration = new Date(new Date().getTime() + 3600l * 1000 * 24 * 365 * 10);
-        String url = ossClient.generatePresignedUrl("ljqzq", name, expiration).toString();
+        String url = ossClient.generatePresignedUrl("xxxxx", name, expiration).toString();
         // 关闭client
         ossClient.shutdown();
         return url;
